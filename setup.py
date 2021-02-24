f527c33600ac2e26f0119bafd0edf61efc228ca7
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-02-24 19:04:53 +0100

    release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 
 setup(name="embryoseg",
-      version='1.0.0',
+      version='1.0.1',
       author='Varun Kapoor',
       author_email='randomaccessiblekapoor@gmail.com',
       url='https://github.com/kapoorlab/EmbryoSeg/',
@@ -14,7 +14,7 @@ setup(name="embryoseg",
       long_description=long_description,
       long_description_content_type='text/markdown',
       install_requires=["numpy", "pandas", "napari==0.4.3", "pyqt5", "btrack","natsort", "scikit-image", "scipy", "opencv-python-headless", "tifffile", "matplotlib", "ffmpeg-python", "imageio_ffmpeg"],
-      packages=['utils','notebooks','models','Images' ],
+      packages=['embryoseg','embryoseg/utils','embryoseg/notebooks','embryoseg/models'],
       classifiers=['Development Status :: 3 - Alpha',
                    'Natural Language :: English',
                    'License :: OSI Approved :: MIT License',
