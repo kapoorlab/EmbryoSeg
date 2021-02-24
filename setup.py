867c8ee05044781980eed96f11ca42ee3f3ed1ce
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-02-24 19:20:27 +0100

    python package release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 
 setup(name="embryoseg",
-      version='1.0.1',
+      version='1.0.2',
       author='Varun Kapoor',
       author_email='randomaccessiblekapoor@gmail.com',
       url='https://github.com/kapoorlab/EmbryoSeg/',
