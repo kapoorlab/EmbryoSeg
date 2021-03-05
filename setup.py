fbeded3570e386beb88388ea569ecdc745b29969
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-03-05 13:32:49 +0100

    local stardist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 
 setup(name="embryoseg",
-      version='1.1.9',
+      version='1.2.0',
       author='Varun Kapoor',
       author_email='randomaccessiblekapoor@gmail.com',
       url='https://github.com/kapoorlab/EmbryoSeg/',
@@ -14,7 +14,7 @@ setup(name="embryoseg",
       long_description=long_description,
       long_description_content_type='text/markdown',
       install_requires=[ "numpy", "pandas", "napari","pyqt5", "natsort", "scikit-image", "scipy", "opencv-python-headless", "tifffile", "matplotlib", "csbdeep"],
-      packages=['embryoseg','embryoseg/utils','embryoseg/stardist_kapoor'],
+      packages=['embryoseg','embryoseg/utils','embryoseg/stardist_kapoor','embryoseg/stardist_kapoor/geometry','embryoseg/stardist_kapoor/kernels','embryoseg/stardist_kapoor/lib', 'embryoseg/stardist_kapoor/lib/qhull_src', 'embryoseg/stardist_kapoor/lib/qhull_src/src/libqhull_r', 'embryoseg/stardist_kapoor/lib/qhull_src/src/libqhullcpp','embryoseg/stardist_kapoor/models'],
       classifiers=['Development Status :: 3 - Alpha',
                    'Natural Language :: English',
                    'License :: OSI Approved :: MIT License',
