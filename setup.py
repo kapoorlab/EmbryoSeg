6f0c352c4a28fe7297a15255ee2c38004db158ff
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-06-09 00:23:06 +0200

    leo's code

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
 from setuptools import setup
 from setuptools import find_packages
+import setuptools
 
 with open('README.md') as f:
     long_description = f.read()
 
 
 setup(name="embryoseg",
-<<<<<<< HEAD
-      version='1.0.3',
-=======
-      version='1.2.4',
->>>>>>> 66610c015a2c23d790167dd23cda7fc6abb9dbaf
-      author='Varun Kapoor',
+      version='2.2.4',
+      author='Varun Kapoor Leo Guginard',
       author_email='randomaccessiblekapoor@gmail.com',
       url='https://github.com/kapoorlab/EmbryoSeg/',
-      description='SmartSeed Segmentation for Mouse Pre-implantation cells.',
+      description='SmartSeed Segmentation for animal embryonic cells.',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      install_requires=["numpy", "pandas", "napari==0.4.3", "pyqt5", "natsort", "scikit-image", "scipy", "opencv-python-headless", "tifffile", "matplotlib"],
-      packages=['embryoseg','embryoseg/utils','embryoseg/notebooks','embryoseg/models'],
+      install_requires=[ "numpy", "pandas", "napari","pyqt5", "natsort", "scikit-image", "scipy", "opencv-python-headless", "tifffile", "matplotlib", "csbdeep", "stardist"],
+      packages= setuptools.find_packages(),
       classifiers=['Development Status :: 3 - Alpha',
                    'Natural Language :: English',
                    'License :: OSI Approved :: MIT License',
